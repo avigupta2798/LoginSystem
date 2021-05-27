74d8683c192f52aa8e140c0ef6c3fab4bdfbb604
Avi Gupta <32445591+avigupta2798@users.noreply.github.com>
2021-05-27 21:11:04 +0530

    Update settings.py

--- LoginSystem/settings.py
+++ LoginSystem/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'LoginSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB',
-        'USER' : 'root',
-        'PASSWORD' : 'root',
-        'HOST' : 'localhost',
-        'PORT' : '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
